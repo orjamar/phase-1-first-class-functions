c92770d6f069574a61808b32aac20ae44b239893
Orlando Martinez <orjamar@gmail.com>
2023-03-26 18:41:31 -0500

    Added first class functions

---
+++ index.js
@@ -0,0 +1,11 @@
+function receivesAFunction(callback) {
+    callback();
+}
+
+function returnsANamedFunction() {
+    return function named() {};
+}
+
+function returnsAnAnonymousFunction() {
+    return function () {};
+}
